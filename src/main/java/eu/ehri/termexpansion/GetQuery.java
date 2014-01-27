378134623986398b9b279ef404cc8a178848659d
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-01-27 13:32:08 +0100

    get the user query

---
+++ src/main/java/eu/ehri/termexpansion/GetQuery.java
@@ -0,0 +1,15 @@
+package eu.ehri.termexpansion;
+
+public class GetQuery {
+
+	private String query;
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+}
