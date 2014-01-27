2a7f0ebfc7d08669366aeb1b0710603f1b6616dd
KepaJRodriguez <kepa.rodriguez@gmail.com>
2014-01-27 13:32:52 +0100

    output result

---
+++ src/main/java/eu/ehri/termexpansion/Result.java
@@ -0,0 +1,24 @@
+package eu.ehri.termexpansion;
+
+public class Result {
+
+	private String result;
+
+	public Result() {
+		
+	}
+	
+	public Result(String obj) {
+		result = obj;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+
+}
